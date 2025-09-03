52c073f92d56504220658895597508fc6654bdd6
Igor Nunes <igornribeiro@hotmail.com>
2025-09-02 21:51:13 -0300

    Aula 02/09 | Exemplos ex4

---
+++ livrojs/cap04/js/ex4_5.js
@@ -0,0 +1,17 @@
+const frm = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+frm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+
+    const numero = Number(frm.inNumero.value)
+    const raiz = Math.sqrt(numero) // calcula raiz quadrada do numero
+
+    if(Number.isInteger(raiz)){   // se valor da raiz for um número inteiro
+        resp.innerText = `Raiz: ${raiz}`  // ....mostra a raiz
+    }else {
+        resp.innerText = `Não há raiz exata para ${numero}`  // ...mostra mensagem
+    }
+
+
+})
