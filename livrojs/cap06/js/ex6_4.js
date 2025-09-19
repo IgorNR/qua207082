fab990a726de2a87f232dd9a7359901057580bf7
Igor Nunes <igornribeiro@hotmail.com>
2025-09-18 21:06:21 -0300

    Aula 18/09 | Exemplo 6.4

--- livrojs/cap06/js/ex6_4.js
+++ livrojs/cap06/js/ex6_4.js
@@ -8,7 +8,7 @@ criancas.push({nome:"Ana", idade:3})
 criancas.push({nome:"Bianca", idade:6})
 criancas.push({nome:"Pedro", idade:6})
 criancas.push({nome:"João", idade:4})
-criancas.push({nome:"Marina", idade:4})
+criancas.push({nome:"Maria", idade:4})
 criancas.push({nome:"Juliana", idade:3})
 criancas.push({nome:"Cátia", idade:6})
 
@@ -45,5 +45,50 @@ frm.addEventListener("submit", (e) => {
 })
 
 frm.btResumir.addEventListener("click", ()=>{ 
-    
+    // verificar se a lista está zerado ?
+    if(criancas == 0 ){
+        alert("Não há crianças na lista")
+    }
+
+    // criar a copia do vetor criança
+    const copia = [...criancas]
+
+    // ordernar pela idade
+    copia.sort((a,b) => a.idade - b.idade)
+
+    // criar uma variavel para concatenar o retorno
+    let resumo = "" 
+
+    // salvar a menor idade do vetor
+    let aux = copia[0].idade
+
+    // criar um vetor para gravar a lista dos nomes por idade
+    let nomes = []
+
+    // iniciar um loop para percorrer o vetor de cópia
+    for(const crianca of copia){        
+        const {nome, idade} = crianca //desestruturação
+
+        // verificar se a idade permanece igual 
+        if(aux == idade){
+        // se sim incluir o nome da criança    
+        nomes.push(nome)
+        }else{
+        // senão montar resumo para cada idade 
+        resumo += `${aux} ano(s): ${nomes.length} criança(s) - `
+        resumo += ((nomes.length/copia.length)*100).toFixed(2)+"%\n"
+        resumo += `(${nomes.join(", ")})\n\n`
+        // atualiza a idade aux
+        aux = idade
+        // limpa o vetor nomes
+        nomes = []
+        // adiciona o primeiro nome da nova idade
+        nomes.push(nome)
+                
+        } 
+    }
+    resumo += `${aux} ano(s): ${nomes.length} criança(s) - `
+    resumo += ((nomes.length/copia.length)*100).toFixed(2)+"%\n"
+    resumo += `(${nomes.join(", ")})\n\n`
+    resp.innerText = resumo
 })
